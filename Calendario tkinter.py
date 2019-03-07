3a9ae932574b755cb59826892c344832273de7be
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-07 21:09:31 +0100

    :(

--- Calendario tkinter.py
+++ Calendario tkinter.py
@@ -1,14 +1,40 @@
 from tkinter import *
 from tkinter import ttk
-from datetime import *
+import datetime
 import calendar
 
 
-"""class Month()
+
+"""class Month():
+    dates:
+    moth: actual.month
+    year: actual.year
+    def setdates():
+    
 class date():
+    date:
+    actual.date
+    active:"True or False"
+    weekend:
+    setdate(date):
+    setactive(True/False):
+    
 class Calendar(ttk.Frame):
-b= calendar.itermonthdates(1,12)
-print(b)"""
+    header: 
+    daysName:
+    strmonth:
+    activeYear:
+    activemonth:
+    def __init__():
+    def backYear:
+    def backMonth:
+    def forYear:
+    def forMonth:"""
+b = tuple(calendar.month_name)
+a = tuple(calendar.day_abbr)
+print(a)
+print(b)
+mesactual=(b[1])
 
 class Mainapp(Tk):
     __strmes = None
@@ -22,13 +48,20 @@ class Mainapp(Tk):
         self.Header()
         self.diassemana()
     def Header(self):
-        self.label = ttk.Label(self, text="mes a introducir")
+        self.label = ttk.Label(self, text=(mesactual))
         self.label.pack()
         self.bt1 = ttk.Button(self, text=" << ").place(x=24.5, y=5)
         self.bt1 = ttk.Button(self, text=" < ").place(x=83.5, y=5)
         self.bt1 = ttk.Button(self, text=" > ").place(x=398.5, y=5)
         self.bt1 = ttk.Button(self, text=" >> ").place(x=457.5, y=5)
-    def diassemana(self):        
+    def diassemana(self, **args):
+        """cont=0
+        sitio=0
+        while len(a) > cont:
+            Label(self, text=a[cont]).place(x=sitio, y=38)
+            sitio +=76
+            cont +=1"""
+        
         self.lunes = ttk.Label(self, text="lunes").place(x=0, y=38)
         self.martes = ttk.Label(self, text="martes").place(x=76, y=38)
         self.miercoles = ttk.Label(self, text="miercoles").place(x=152, y=38)

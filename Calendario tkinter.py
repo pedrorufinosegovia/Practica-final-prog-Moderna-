4bcfbde31d607428de1dedb8641d14101b334718
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-09 10:50:58 +0100

    avanceconsiderable

--- Calendario tkinter.py
+++ Calendario tkinter.py
@@ -1,12 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 import datetime
+from datetime import date, datetime 
 import calendar
-
-
+ahora = datetime.now()
+meses = tuple(calendar.month_name)
+diassemana = tuple(calendar.day_abbr)
+intmesactual=ahora.month
+añoactual=ahora.year
+strmesactual = meses[ahora.month]
 
 """class Month():
-    dates:
+    mesactual =
     moth: actual.month
     year: actual.year
     def setdates():
@@ -17,61 +22,66 @@ class date():
     active:"True or False"
     weekend:
     setdate(date):
-    setactive(True/False):
+    setactive(True/False):"""
     
 class Calendar(ttk.Frame):
-    header: 
-    daysName:
-    strmonth:
-    activeYear:
-    activemonth:
-    def __init__():
-    def backYear:
-    def backMonth:
-    def forYear:
-    def forMonth:"""
-b = tuple(calendar.month_name)
-a = tuple(calendar.day_abbr)
-print(a)
-print(b)
-mesactual=(b[1])
-
-class Mainapp(Tk):
     __strmes = None
     __mes = None
     __mesmas = None
+    meses = tuple(calendar.month_name)
+    intmesactual=ahora.month
+    añoactual=ahora.year
+    strmesactual = meses[ahora.month]
+    ahora = datetime.now()
+    def __init__(self, parent, **args):
+        ttk.Framne.__init__(self, parent, width=532, heigth=422)
+        self.Botones()
+        self.Diassemana()
+        
+    def Botones(self):
+        self.cabecera = ttk.Frame(self, width=532, heigth=40).place(x=0,y=0)
+        self.anhomenos = ttk.Button(self.cabecera, text=" << ")
+        self.anhomenos.place(x=24.5, y=5)
+        self.mesmenos = ttk.Button(self.cabecera, text=" < ")
+        self.mesmenos.place(x=83.5, y=5)
+        self.mesmas = ttk.Button(self.cabecera, text=" > ")
+        self.mesmas.place(x=398.5, y=5)
+        self.anhomas = ttk.Button(self.cabecera, text=" >> ")
+        self.anhomas.place(x=457.5, y=5)
+        self.label = ttk.Label(self.cabecera, text=(self.strmesactual, self.añoactual))
+        self.label.pack()
+        
+    def Diassemnana(self):
+        self.dias = ttk.Frame(self, width = 532, heigth= 20).place(x=0, y=38)
+        self.lunes = ttk.Label(self, text="lunes", font=("Arial", 11)).place(x=0, y=38)
+        self.martes = ttk.Label(self, text="martes", font=("Arial", 11)).place(x=76, y=38)
+        self.miercoles = ttk.Label(self, text="miercoles", font=("Arial", 11)).place(x=152, y=38)
+        self.jueves = ttk.Label(self, text="jueves", font=("Arial", 11)).place(x=228, y=38)
+        self.viernes = ttk.Label(self, text="viernes", font=("Arial", 11)).place(x=304, y=38)
+        self.sabado = ttk.Label(self, text="sabado", font=("Arial", 11)).place(x=380, y=38)
+        self.domingo = ttk.Label(self, text="domingo", font=("Arial", 11)).place(x=456, y=38)
+
+class Mainapp(Tk):
+    """__strmes = None
+    __mes = None
+    __mesmas = None
+    meses = tuple(calendar.month_name)
+    intmesactual=ahora.month
+    añoactual=ahora.year
+    strmesactual = meses[ahora.month]
+    ahora = datetime.now()"""
+    calendar = Calendar()
+    calendar.__init__()
     def __init__(self):
         Tk.__init__(self)
         self.title("Calendario Universal")
         self.geometry("532x422")
         self.configure(bg = "#DED7B9")
-        self.Header()
-        self.diassemana()
-    def Header(self):
-        self.label = ttk.Label(self, text=(mesactual))
-        self.label.pack()
-        self.bt1 = ttk.Button(self, text=" << ").place(x=24.5, y=5)
-        self.bt1 = ttk.Button(self, text=" < ").place(x=83.5, y=5)
-        self.bt1 = ttk.Button(self, text=" > ").place(x=398.5, y=5)
-        self.bt1 = ttk.Button(self, text=" >> ").place(x=457.5, y=5)
-    def diassemana(self, **args):
-        """cont=0
-        sitio=0
-        while len(a) > cont:
-            Label(self, text=a[cont]).place(x=sitio, y=38)
-            sitio +=76
-            cont +=1"""
-        
-        self.lunes = ttk.Label(self, text="lunes").place(x=0, y=38)
-        self.martes = ttk.Label(self, text="martes").place(x=76, y=38)
-        self.miercoles = ttk.Label(self, text="miercoles").place(x=152, y=38)
-        self.jueves = ttk.Label(self, text="jueves").place(x=228, y=38)
-        self.viernes = ttk.Label(self, text="viernes").place(x=304, y=38)
-        self.sabado = ttk.Label(self, text="sabado").place(x=380, y=38)
-        self.domingo = ttk.Label(self, text="domingo").place(x=456, y=38)
+        self.calendar = Calendar(self)
+        self.calendar.__init__()
 
     def start(self):
-            self.mainloop()
+        self.mainloop()
 
 
 
